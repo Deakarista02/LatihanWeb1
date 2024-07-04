da531742521e469c358a4b202f72b14f4c7da496
DeaKarista02 <dea@atudent.undiksha.ac.id>
2024-07-05 00:11:34 +0800

    first commit

---
+++ web/index.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h2>HALO SEMUANYA!</h2>";
+    echo "Perkenalkan nama saya A. A. Dea Karista Dewi. Saya merupakan mahasiswa di Universitas Pendidikan Ganesha Prodi Ilmu Komputer Semester 4. Saat ini saya sedang belajar Docker";
+?>
